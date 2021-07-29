7f274655246e43f144258c87a280d22ea495e9e8
marcusformiga <kiko.antares@gmail.com>
2021-07-29 10:51:53 -0300

    feature: criação do serviço para exibição de todos os produtos

---
+++ src/modules/products/services/ListProductsService.ts
@@ -0,0 +1,11 @@
+import { Product } from '../typeorm/entities/Product'
+import { ProductsRepository } from '../typeorm/repository/ProductRepository'
+import { getCustomRepository } from 'typeorm'
+
+export class ListProductsService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductsRepository)
+    const products = await productsRepository.find()
+    return products
+  }
+}
