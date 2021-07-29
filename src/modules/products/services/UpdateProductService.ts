805f9170b03bec8e19fd1e1406e986a26cf52b05
marcusformiga <kiko.antares@gmail.com>
2021-07-29 11:17:33 -0300

    feature: criação do serviço de atualização de produtos

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import { AppError } from '../../../shared/errors/AppError'
+import { Product } from '../typeorm/entities/Product'
+import { ProductsRepository } from '../typeorm/repository/ProductRepository'
+import { getCustomRepository } from 'typeorm'
+
+interface IRequest {
+  id: string
+  name: string
+  price: number
+  quantity: number
+}
+export class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product | undefined> {
+    const productRepository = getCustomRepository(ProductsRepository)
+    const product = await productRepository.findById(id)
+    const productExists = await productRepository.findByName(name)
+    if (!product) {
+      throw new AppError(`Produto com id ${id} informado não existe`, 400)
+    }
+    if (productExists) {
+      throw new AppError(
+        `Produto com o nome ${name} informado já existe, impossível atualizar`,
+        400,
+      )
+    }
+    product.name = name
+    product.price = price
+    product.quantity = quantity
+    const updateProduct = productRepository.create({
+      name,
+      price,
+      quantity,
+    })
+    await productRepository.save(updateProduct)
+    return updateProduct
+  }
+}
