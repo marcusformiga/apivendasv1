f7e170b18c98cb9e8fecc5802be4b754f9a37b95
marcusformiga <kiko.antares@gmail.com>
2021-07-29 15:00:56 -0300

    feature: criação do serviço para criação de usuario

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,28 @@
+import { AppError } from "../../../shared/errors/AppError"
+import { User } from "../typeorm/entities/User"
+import { UserRepository } from "../typeorm/repository/Users"
+import { getCustomRepository } from "typeorm"
+
+interface IRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class CreateUserService {
+  public async execute({
+    name,
+    email,
+    password,
+  }: IRequest): Promise<User | undefined> {
+    const userRepository = getCustomRepository(UserRepository)
+    const emailExists = await userRepository.findByEmail(email)
+    if (emailExists) {
+      // eslint-disable-next-line prettier/prettier
+      throw new AppError(`Usuário com email ${email} informado já está cadastrado`, 409)
+    }
+    const user = userRepository.create({ name, email, password })
+    await userRepository.save(user)
+    return user
+  }
+}
