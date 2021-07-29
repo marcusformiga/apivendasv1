417c05a5506439e4140fdc4978d810c810f56dfe
marcusformiga <kiko.antares@gmail.com>
2021-07-29 14:07:30 -0300

    fix: arrumando bug das rotas

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,11 +1,29 @@
-import { Router } from 'express'
-import { ProductController } from '../controllers/ProductControllers'
-
+import { Router } from "express"
+import { ProductController } from "../controllers/ProductControllers"
+import { celebrate, Joi, Segments } from "celebrate"
 export const productRouter = Router()
 const productController = new ProductController()
 
-productRouter.get('/', productController.listAll)
-productRouter.post('/', productController.create)
-productRouter.get('/:id', productController.listOne)
-productRouter.put('/:id', productController.update)
-productRouter.delete('/:id', productController.remove)
+productRouter.get("/", productController.listAll)
+productRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().required().precision(2),
+      quantity: Joi.number().integer().required(),
+    },
+  }),
+  productController.create,
+)
+productRouter.get(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.listOne,
+)
+productRouter.put("/:id", productController.update)
+productRouter.delete("/:id", productController.remove)
