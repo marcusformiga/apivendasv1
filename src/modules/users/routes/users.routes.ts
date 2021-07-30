ec8a01004df2f43e977821b5ed5ed769516f0851
marcusformiga <kiko.antares@gmail.com>
2021-07-30 08:27:04 -0300

    refactor: adição de validação nas rotas de usuario

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express"
 import { UserController } from "../controllers/UserController"
+import { Joi, Segments, celebrate } from "celebrate"
 
 export const usersRouter = Router()
 const userController = new UserController()
 
 usersRouter.get("/", userController.listAll)
-usersRouter.post("/", userController.create)
-
+usersRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create,
+)
