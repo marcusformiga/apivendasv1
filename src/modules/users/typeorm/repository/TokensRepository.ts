3e0046e873f58f01909a6e0854a174496d5972ee
marcusformiga <kiko.antares@gmail.com>
2021-07-30 15:17:57 -0300

    feature: criação da entidade de tokens e repositorio de tokens

---
+++ src/modules/users/typeorm/repository/TokensRepository.ts
@@ -0,0 +1,25 @@
+import { Entity, EntityRepository, Repository } from "typeorm"
+import { UserToken } from "../entities/UserToken"
+
+@EntityRepository(UserToken)
+export class TokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: { token },
+    })
+    return userToken
+  }
+  public async findById(id: string): Promise<UserToken | undefined> {
+    const user = await this.findOne({
+      where: { id },
+    })
+    return user
+  }
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = await this.create({
+      user_id,
+    })
+    await this.save(userToken)
+    return userToken
+  }
+}
