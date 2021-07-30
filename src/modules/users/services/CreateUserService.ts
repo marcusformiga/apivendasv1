da1e20d73715c32f52fc362544580360f0019d44
marcusformiga <kiko.antares@gmail.com>
2021-07-30 08:48:25 -0300

    refactor: hash de senha de usuarios

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { AppError } from "../../../shared/errors/AppError"
 import { User } from "../typeorm/entities/User"
 import { UserRepository } from "../typeorm/repository/Users"
 import { getCustomRepository } from "typeorm"
+import bcrypt from "bcrypt"
 
 interface IRequest {
   name: string
@@ -21,7 +22,8 @@ export class CreateUserService {
       // eslint-disable-next-line prettier/prettier
       throw new AppError(`Usuário com email ${email} informado já está cadastrado`, 409)
     }
-    const user = userRepository.create({ name, email, password })
+    const hashPassword = await bcrypt.hash(password, 10)
+    const user = userRepository.create({ name, email, password: hashPassword })
     await userRepository.save(user)
     return user
   }
