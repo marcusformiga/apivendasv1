d81ccfb8c4e2fa04695cb4047bb0e5198e375f53
marcusformiga <kiko.antares@gmail.com>
2021-07-30 09:34:33 -0300

    feature: criação do middleware para proteção de rotas de usuarios

---
+++ src/shared/middlewares/isAuthenticated.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express"
+import { AppError } from "../errors/AppError"
+import jwt from "jsonwebtoken"
+import { authJwt } from "../config/auth"
+
+export function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization
+  if (!authHeader) {
+    throw new AppError("Não existe token jwt", 401)
+  }
+  const [bearer, token] = authHeader.split(" ")
+  try {
+    const decodedToken = jwt.verify(token, authJwt.jwt.secret)
+    return next()
+  } catch {
+    throw new AppError(`Token jwt ${token} inválido`, 401)
+  }
+}
