35eb7887966707e13a777e8fc846b9bdb219ab91
marcusformiga <kiko.antares@gmail.com>
2021-07-31 15:42:44 -0300

    refactor: adicionando etherealmail ao serviço de recuperação de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { AppError } from "../../../shared/errors/AppError"
 import { UserRepository } from "../typeorm/repository/Users"
 import { TokensRepository } from "../typeorm/repository/TokensRepository"
 import { getCustomRepository } from "typeorm"
+import { EtherealMail } from "../../../shared/config/mail/EtherealMail"
 
 interface IRequest {
   email: string
@@ -16,6 +17,9 @@ export class SendForgotPasswordEmailService {
       throw new AppError(`Usuário com email informado não existe`, 404)
     }
     const token = await tokenRepository.generate(user.id)
-    console.log(token)
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefiniçao de senha: ${token.token}`,
+    })
   }
 }
