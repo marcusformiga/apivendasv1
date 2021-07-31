0e9c2f41871071522563720b91ada5702aff2152
marcusformiga <kiko.antares@gmail.com>
2021-07-31 13:50:13 -0300

    fix: correção de bug na request

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -1,9 +1,10 @@
-import { request, Request, Response } from "express"
+import { Request, Response } from "express"
 import { CreateSessionsService } from "../services/CreateSessionsService"
 
 export class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body
+    console.log(email, password)
     const createSession = new CreateSessionsService()
     const session = await createSession.execute({ email, password })
     return response.json(session).status(201)
