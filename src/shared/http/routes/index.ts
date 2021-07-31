e6dddfbdd1d6c17f123eb91e83937f4351f9c2f0
marcusformiga <kiko.antares@gmail.com>
2021-07-31 17:31:24 -0300

    refactor: movendo rota de usuario para arquivo principal

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,9 +3,11 @@ import { productRouter } from "../../../modules/products/routes/products.routes"
 import { usersRouter } from "../../../modules/users/routes/users.routes"
 import { sessionsRouter } from "../../../modules/users/routes/sessions.routes"
 import { passwordRoutes } from "../../../modules/users/routes/password.routes"
+import { profileRouter } from "../../../modules/users/routes/profile.routes"
 export const routes = Router()
 
 routes.use("/products", productRouter)
 routes.use("/users", usersRouter)
 routes.use("/sessions", sessionsRouter)
 routes.use("/password", passwordRoutes)
+routes.use("/profile", profileRouter)
