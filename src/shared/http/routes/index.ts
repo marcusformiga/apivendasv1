1168d33a433fd74e80db4135cee788b26dd8afab
marcusformiga <kiko.antares@gmail.com>
2021-07-31 15:04:12 -0300

    refactor: adição de rota de password

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express"
 import { productRouter } from "../../../modules/products/routes/products.routes"
 import { usersRouter } from "../../../modules/users/routes/users.routes"
 import { sessionsRouter } from "../../../modules/users/routes/sessions.routes"
+import { passwordRoutes } from "../../../modules/users/routes/password.routes"
 export const routes = Router()
 
 routes.use("/products", productRouter)
 routes.use("/users", usersRouter)
 routes.use("/sessions", sessionsRouter)
+routes.use("/password", passwordRoutes)
