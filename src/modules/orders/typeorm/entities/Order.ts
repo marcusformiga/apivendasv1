289aebc35f9e67077a5eeed6a24c6668816af975
marcusformiga <kiko.antares@gmail.com>
2021-08-01 19:30:51 -0300

    feature: criação da entidade order

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,22 @@
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm"
+import { Customer } from "../../../customers/typeorm/entities/Customer"
+
+@Entity("orders")
+export class Order {
+  @PrimaryGeneratedColumn("uuid")
+  id: string
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: "customer_id" })
+  customer: Customer
+  @CreateDateColumn()
+  created_at: Date
+  @UpdateDateColumn()
+  updated_at: Date
+}
